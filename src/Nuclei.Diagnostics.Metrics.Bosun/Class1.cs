b4792976fa181da66b4aed4a5c3b3eb16e87bc96
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-08-29 22:00:00 +1200

    Adding the solution and the projects
    references #1

---
+++ src/Nuclei.Diagnostics.Metrics.Bosun/Class1.cs
@@ -0,0 +1,19 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="TheNucleus">
+// Copyright (c) TheNucleus. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENCE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuclei.Diagnostics.Metrics.Bosun
+{
+    public class Class1
+    {
+    }
+}
